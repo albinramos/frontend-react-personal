49133bc173bcc0d5f1f4ae69bcacbb9cceb2b517
albinramos <dramosh79@gmail.com>
2023-11-26 21:44:40 +0100

    primer commit

---
+++ src/Data.jsx
@@ -0,0 +1,110 @@
+
+import Project1 from './assets/project1.png';
+import Project2 from './assets/project2.png';
+import Project3 from './assets/project3.png';
+import Project4 from './assets/project4.png';
+
+
+
+export const services = [
+    {
+        id:1,
+        name: 'Web Development',
+        titel: 'Web Design & Log',
+        description: 'Wed designers craft the overall vision & plan for a website layout. Professional logo development: Business, Company or Personal.'
+    },
+    {
+        id:2,
+        name: 'Apps Dvelopment',
+        title: 'iOS & Android',
+        description: 'Design Software applications to run on mobile devices. Modern and mobile-ready application that will help you reach all of your marketing.'
+    },
+    {
+        id:3,
+        name:'Game Development',
+        description:'Creating games & describes the design, development and release of a game. Developing unique mobile android and ios games.'
+    },
+    {
+        id:4,
+        name:'Advertising',
+        title:'Google Ads',
+        description: 'Advertising services include: Google search result pages, gmails, Youtube and other webiste participated in Google Ads program.'
+    },
+    {
+        id:5,
+        name: 'Music Writing',
+        title: 'Sound Track',
+        description: 'Music copying, writing, creating, transcription and composition services.'
+    },
+];
+
+export const skills = [
+    {
+        id:1,
+        name: 'PHP',
+        percentage: 50,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+        id:2,
+        name: 'Javascript',
+        percentage: 70,
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+        id:3,
+        name: 'CSS',
+        percentage: 80,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+        id:4,
+        name: 'React',
+        percentage: 70,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+        id:5,
+        name: 'React',
+        percentage: 70,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+        id:6,
+        name: 'React',
+        percentage: 70,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    }
+
+]
+
+export const projects = [
+    {
+        id:1,
+        img: Project1,
+        category: 'Personal Website',
+        title: 'RRR',
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+        id:2,
+        img: Project2,
+        category: 'Company Website',
+        title: 'Arra',
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+        id:3,
+        img: Project3,
+        category: 'Museum Website',
+        title: 'Museum Search',
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+        id:4,
+        img: Project4,
+        category: 'Website',
+        title: 'Ejemplo',
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    }
+]
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import profileImg from "../../assets/Happy-Person-PNG-Picture.png";
+import shapeOne from '../../assets/shape-1.png';
+import shapeTwo from '../../assets/shape-2.png';
+
+import './home.css';
+
+import { FaFacebook, FaInstagram, FaBehance } from "react-icons/fa";
+
+
+const Home = () => {
+    return (
+    <section className="home" id='home'>
+        <div className="home__wrapper">
+        <div className="home__container container">
+            <p className="home__subtitle text-cs">
+                Hello, <span>My Name Is</span>
+            </p>
+
+            <h1 className="home__title text-cs"><span>DAVID</span> RAMOS</h1>
+
+            <p className="home__job">
+                <span className="text-cs">I Am</span><b>Web Developer</b>
+            </p>
+
+            <div className="home__img-wrapper">
+                <div className="home__banner">
+                    <img src={profileImg} alt="profile img" className="home__profile" />
+                </div>
+
+                <p className="home__data home__data-one">
+                    <span className="text-lg">
+                        12<b>+</b>
+                    </span>
+                    <span className="text-sm text-cs">
+                        Years of <span>Experience</span>
+                    </span>
+                </p>
+
+                <p className="home__data home__data-two">
+                    <span className="text-lg">
+                        330
+                    </span>
+                    <span className="text-sm text-cs">
+                        Completed<br>
+                        </br><span>
+                            Projects</span>
+                    </span>
+                </p>
+                 <img src={shapeOne} alt="shape one" className="shape shape__1" />
+                 <img src={shapeTwo} alt="shape two" className="shape shape__2" />
+            </div>
+
+            <p className="home__text">
+                From Bilbao. I am a junior web designer. I would like to talk to you.
+            </p>
+            <div className="home__socials">
+                <a href='' className="home__social-link">
+                    <FaInstagram />
+                </a>
+                <a href='' className="home__social-link">
+                <FaFacebook />
+                </a>
+                <a href='' className="home__social-link">
+                <FaBehance />
+                </a>
+            </div>
+            <div className="home__btns">
+                <a href="" className="btn">
+                    Download CV
+                </a>
+                <a href="" className="hero__link text-cs">
+                    My Skills
+                </a>
+            </div>
+            </div>
+        </div>
+
+    </section>
+    );
+};
+
+export default Home
